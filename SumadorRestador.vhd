2bfa2eee6f17d31a091bc82c00bd80a9302a858b
Yago Tobio Souto <59061041+SaltyYagi123@users.noreply.github.com>
2021-03-21 17:10:18 +0100

    Reconsideracion de fases del project

--- SumadorRestador.vhd
+++ SumadorRestador.vhd
@@ -6,6 +6,7 @@ ENTITY sumadorRestador IS
     PORT (
         a, b : IN STD_LOGIC_VECTOR (32 DOWNTO 0); --entradas
         --No hay un overflow por lo tanto extendemos los datos a 33 bits 
+
         s : OUT STD_LOGIC_VECTOR (31 DOWNTO 0); --salida del numero
         sig : OUT STD_LOGIC;
         s_r : IN STD_LOGIC_VECTOR(1 DOWNTO 0) --Numero que determina si suma, resta o compara 
@@ -15,7 +16,7 @@ END sumadorRestador5Bits;
 
 ARCHITECTURE structural OF sumadorRestador IS
 
-    SIGNAL 
+    SIGNAL
 
 BEGIN
 
